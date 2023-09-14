crazy-stuffs/vendor_support

support: Import SelfRemovingPreference from LineageOS
Signed-off-by: Mohammad Hasan Keramat J <ikeramat@protonmail.com>
Change-Id: I460c64a92ce7663ebf7f43780adcc337c0bbf433

src/com/libremobileos/support/preferences/SelfRemovingDropDownPreference.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.libremobileos.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.DropDownPreference;
+import androidx.preference.PreferenceDataStore;
+import androidx.preference.PreferenceViewHolder;
+
+/**
+ * A Preference which can automatically remove itself from the hierarchy
+ * based on constraints set in XML.
+ */
+public abstract class SelfRemovingDropDownPreference extends DropDownPreference {
+
+    private final ConstraintsHelper mConstraints;
+
+    public SelfRemovingDropDownPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingDropDownPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingDropDownPreference(Context context) {
+        super(context);
+        mConstraints = new ConstraintsHelper(context, null, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
+    }
+
+    protected abstract boolean isPersisted();
+    protected abstract void putString(String key, String value);
+    protected abstract String getString(String key, String defaultValue);
+
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        final String value;
+        if (!restorePersistedValue || !isPersisted()) {
+            if (defaultValue == null) {
+                return;
+            }
+            value = (String) defaultValue;
+            if (shouldPersist()) {
+                persistString(value);
+            }
+        } else {
+            // Note: the default is not used because to have got here
+            // isPersisted() must be true.
+            value = getString(getKey(), null /* not used */);
+        }
+        setValue(value);
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putString(String key, String value) {
+            SelfRemovingDropDownPreference.this.putString(key, value);
+        }
+
+        @Override
+        public String getString(String key, String defaultValue) {
+            return SelfRemovingDropDownPreference.this.getString(key, defaultValue);
+        }
+    }
+}

src/com/libremobileos/support/preferences/SelfRemovingListPreference.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.libremobileos.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.ListPreference;
+import androidx.preference.PreferenceDataStore;
+import androidx.preference.PreferenceViewHolder;
+
+/**
+ * A Preference which can automatically remove itself from the hierarchy
+ * based on constraints set in XML.
+ */
+public abstract class SelfRemovingListPreference extends ListPreference {
+
+    private final ConstraintsHelper mConstraints;
+
+    public SelfRemovingListPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingListPreference(Context context) {
+        super(context);
+        mConstraints = new ConstraintsHelper(context, null, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
+    }
+
+    protected abstract boolean isPersisted();
+    protected abstract void putString(String key, String value);
+    protected abstract String getString(String key, String defaultValue);
+
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        final String value;
+        if (!restorePersistedValue || !isPersisted()) {
+            if (defaultValue == null) {
+                return;
+            }
+            value = (String) defaultValue;
+            if (shouldPersist()) {
+                persistString(value);
+            }
+        } else {
+            // Note: the default is not used because to have got here
+            // isPersisted() must be true.
+            value = getString(getKey(), null /* not used */);
+        }
+        setValue(value);
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putString(String key, String value) {
+            SelfRemovingListPreference.this.putString(key, value);
+        }
+
+        @Override
+        public String getString(String key, String defaultValue) {
+            return SelfRemovingListPreference.this.getString(key, defaultValue);
+        }
+    }
+}

src/com/libremobileos/support/preferences/SelfRemovingPreference.java
@@ -1,0 +1,71 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.libremobileos.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+/**
+ * A Preference which can automatically remove itself from the hierarchy
+ * based on constraints set in XML.
+ */
+public class SelfRemovingPreference extends Preference {
+
+    private final ConstraintsHelper mConstraints;
+
+    public SelfRemovingPreference(Context context, AttributeSet attrs,
+                                  int defStyle, int defStyleRes) {
+        super(context, attrs, defStyle, defStyleRes);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+    }
+
+    public SelfRemovingPreference(Context context, AttributeSet attrs, int defStyle) {
+        this(context, attrs, defStyle, 0);
+    }
+
+    public SelfRemovingPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, ConstraintsHelper.getAttr(context,
+                androidx.preference.R.attr.preferenceStyle,
+                android.R.attr.preferenceStyle));
+    }
+
+    public SelfRemovingPreference(Context context) {
+        this(context, null);
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
+    }
+}

src/com/libremobileos/support/preferences/SelfRemovingSwitchPreference.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.libremobileos.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceDataStore;
+import androidx.preference.PreferenceViewHolder;
+import androidx.preference.SwitchPreference;
+
+/**
+ * A SwitchPreference which can automatically remove itself from the hierarchy
+ * based on constraints set in XML.
+ */
+public abstract class SelfRemovingSwitchPreference extends SwitchPreference {
+
+    private final ConstraintsHelper mConstraints;
+
+    public SelfRemovingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingSwitchPreference(Context context) {
+        super(context);
+        mConstraints = new ConstraintsHelper(context, null, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
+    }
+
+    protected abstract boolean isPersisted();
+    protected abstract void putBoolean(String key, boolean value);
+    protected abstract boolean getBoolean(String key, boolean defaultValue);
+
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        final boolean checked;
+        if (!restorePersistedValue || !isPersisted()) {
+            if (defaultValue == null) {
+                return;
+            }
+            checked = (boolean) defaultValue;
+            if (shouldPersist()) {
+                persistBoolean(checked);
+            }
+        } else {
+            // Note: the default is not used because to have got here
+            // isPersisted() must be true.
+            checked = getBoolean(getKey(), false /* not used */);
+        }
+        setChecked(checked);
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putBoolean(String key, boolean value) {
+            SelfRemovingSwitchPreference.this.putBoolean(key, value);
+        }
+
+        @Override
+        public boolean getBoolean(String key, boolean defaultValue) {
+            return SelfRemovingSwitchPreference.this.getBoolean(key, defaultValue);
+        }
+    }
+}

SuperiorOS/android_vendor_support

src/com/superior/support/preferences/SelfRemovingDropDownPreference.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.superior.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.DropDownPreference;
+import androidx.preference.PreferenceDataStore;
+import androidx.preference.PreferenceViewHolder;
+
+/**
+ * A Preference which can automatically remove itself from the hierarchy
+ * based on constraints set in XML.
+ */
+public abstract class SelfRemovingDropDownPreference extends DropDownPreference {
+
+    private final ConstraintsHelper mConstraints;
+
+    public SelfRemovingDropDownPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingDropDownPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingDropDownPreference(Context context) {
+        super(context);
+        mConstraints = new ConstraintsHelper(context, null, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
+    }
+
+    protected abstract boolean isPersisted();
+    protected abstract void putString(String key, String value);
+    protected abstract String getString(String key, String defaultValue);
+
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        final String value;
+        if (!restorePersistedValue || !isPersisted()) {
+            if (defaultValue == null) {
+                return;
+            }
+            value = (String) defaultValue;
+            if (shouldPersist()) {
+                persistString(value);
+            }
+        } else {
+            // Note: the default is not used because to have got here
+            // isPersisted() must be true.
+            value = getString(getKey(), null /* not used */);
+        }
+        setValue(value);
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putString(String key, String value) {
+            SelfRemovingDropDownPreference.this.putString(key, value);
+        }
+
+        @Override
+        public String getString(String key, String defaultValue) {
+            return SelfRemovingDropDownPreference.this.getString(key, defaultValue);
+        }
+    }
+}

src/com/superior/support/preferences/SelfRemovingListPreference.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.superior.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.ListPreference;
+import androidx.preference.PreferenceDataStore;
+import androidx.preference.PreferenceViewHolder;
+
+/**
+ * A Preference which can automatically remove itself from the hierarchy
+ * based on constraints set in XML.
+ */
+public abstract class SelfRemovingListPreference extends ListPreference {
+
+    private final ConstraintsHelper mConstraints;
+
+    public SelfRemovingListPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingListPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingListPreference(Context context) {
+        super(context);
+        mConstraints = new ConstraintsHelper(context, null, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
+    }
+
+    protected abstract boolean isPersisted();
+    protected abstract void putString(String key, String value);
+    protected abstract String getString(String key, String defaultValue);
+
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        final String value;
+        if (!restorePersistedValue || !isPersisted()) {
+            if (defaultValue == null) {
+                return;
+            }
+            value = (String) defaultValue;
+            if (shouldPersist()) {
+                persistString(value);
+            }
+        } else {
+            // Note: the default is not used because to have got here
+            // isPersisted() must be true.
+            value = getString(getKey(), null /* not used */);
+        }
+        setValue(value);
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putString(String key, String value) {
+            SelfRemovingListPreference.this.putString(key, value);
+        }
+
+        @Override
+        public String getString(String key, String defaultValue) {
+            return SelfRemovingListPreference.this.getString(key, defaultValue);
+        }
+    }
+}

src/com/superior/support/preferences/SelfRemovingPreference.java
@@ -1,0 +1,71 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.superior.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceViewHolder;
+
+/**
+ * A Preference which can automatically remove itself from the hierarchy
+ * based on constraints set in XML.
+ */
+public class SelfRemovingPreference extends Preference {
+
+    private final ConstraintsHelper mConstraints;
+
+    public SelfRemovingPreference(Context context, AttributeSet attrs,
+                                  int defStyle, int defStyleRes) {
+        super(context, attrs, defStyle, defStyleRes);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+    }
+
+    public SelfRemovingPreference(Context context, AttributeSet attrs, int defStyle) {
+        this(context, attrs, defStyle, 0);
+    }
+
+    public SelfRemovingPreference(Context context, AttributeSet attrs) {
+        this(context, attrs, ConstraintsHelper.getAttr(context,
+                androidx.preference.R.attr.preferenceStyle,
+                android.R.attr.preferenceStyle));
+    }
+
+    public SelfRemovingPreference(Context context) {
+        this(context, null);
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
+    }
+}

src/com/superior/support/preferences/SelfRemovingSwitchPreference.java
@@ -1,0 +1,106 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2018 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.superior.support.preferences;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.preference.PreferenceDataStore;
+import androidx.preference.PreferenceViewHolder;
+import androidx.preference.SwitchPreference;
+
+/**
+ * A SwitchPreference which can automatically remove itself from the hierarchy
+ * based on constraints set in XML.
+ */
+public abstract class SelfRemovingSwitchPreference extends SwitchPreference {
+
+    private final ConstraintsHelper mConstraints;
+
+    public SelfRemovingSwitchPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingSwitchPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mConstraints = new ConstraintsHelper(context, attrs, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    public SelfRemovingSwitchPreference(Context context) {
+        super(context);
+        mConstraints = new ConstraintsHelper(context, null, this);
+        setPreferenceDataStore(new DataStore());
+    }
+
+    @Override
+    public void onAttached() {
+        super.onAttached();
+        mConstraints.onAttached();
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+        mConstraints.onBindViewHolder(holder);
+    }
+
+    public void setAvailable(boolean available) {
+        mConstraints.setAvailable(available);
+    }
+
+    public boolean isAvailable() {
+        return mConstraints.isAvailable();
+    }
+
+    protected abstract boolean isPersisted();
+    protected abstract void putBoolean(String key, boolean value);
+    protected abstract boolean getBoolean(String key, boolean defaultValue);
+
+    @Override
+    protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+        final boolean checked;
+        if (!restorePersistedValue || !isPersisted()) {
+            if (defaultValue == null) {
+                return;
+            }
+            checked = (boolean) defaultValue;
+            if (shouldPersist()) {
+                persistBoolean(checked);
+            }
+        } else {
+            // Note: the default is not used because to have got here
+            // isPersisted() must be true.
+            checked = getBoolean(getKey(), false /* not used */);
+        }
+        setChecked(checked);
+    }
+
+    private class DataStore extends PreferenceDataStore {
+        @Override
+        public void putBoolean(String key, boolean value) {
+            SelfRemovingSwitchPreference.this.putBoolean(key, value);
+        }
+
+        @Override
+        public boolean getBoolean(String key, boolean defaultValue) {
+            return SelfRemovingSwitchPreference.this.getBoolean(key, defaultValue);
+        }
+    }
+}